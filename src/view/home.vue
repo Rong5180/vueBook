c6611f4181aae5d3897aee871c21a2bad408f046
Rong <2441238004@qq.com>
2020-11-20 11:59:59 +0800

    '图书管理完成'

--- src/view/home.vue
+++ src/view/home.vue
@@ -6,13 +6,17 @@
        <label for="id">
          编号：
        </label>
-       <input type="text" id="id"  v-model="id">
+       <input type="text" id="id"  v-model="id" :disabled="flag" v-focus>
        <label for="name">
          名称：
        </label>
        <input type="text" id="name" v-model="name">
-       <el-button type="danger" size="mini" @click="handle">提交</el-button>
+       <el-button type="danger" size="mini" @click="handle" :disabled="flagSubmit">提交</el-button>
      </div>
+   </div>
+   <div class="total">
+     <span>图书总数：</span>
+     <span>{{total}}</span>
    </div>
    <table>
      <thead>
@@ -27,12 +31,12 @@
      <tr v-for="item in books" :key="item.id">
        <td>{{item.id}}</td>
        <td>{{item.name}}</td>
-       <td>{{item.date}}</td>
+       <td>{{item.date|format}}</td>
        <td>
-         <a href="" @click.prevent>删除</a>
+         <a href="" @click.prevent @click="toEdit(item.id)">修改</a>
 <!--         禁止a标签默认行为-->
          <span> | </span>
-         <a href="" @click.prevent>修改</a>
+         <a href="" @click.prevent @click="deleteBook(item.id)">删除</a>
        </td>
      </tr>
      </tbody>
@@ -47,31 +51,106 @@
     data() {
       return {
         flag:false,
+        flagSubmit:false,
         id:'',
         name:'',
         books:[{
           id:1,
           name:'三国演义',
-          date:''
+          date:new Date()
         },
           {
             id:2,
             name:'水浒传',
-            date:''
+            date:new Date()
           },
           {
             id:3,
             name:'西游记',
-            date:''
+            date:new Date()
           }]
       }
     },
     methods:{
       //添加图书
       handle(){
-      
+        if(this.flag===true)
+        {
+          this.books.some(item=> {
+              if(item.id===this.id)
+              {
+                item.name=this.name;
+                return true;
+              }
+          })
+          this.flag=false;
+         
+        }else {
+          let book = {};
+          book.id = this.id;
+          book.name = this.name;
+          book.date=new Date();
+          this.books.push(book);
+        }
+        
+        this.id='';
+        this.name='';
+      },
+      toEdit(id){
+       let book= this.books.filter(function (item) {
+          return item.id===id;
+        });
+       this.id=book[0].id;
+       this.name=book[0].name;
+       this.flag=true;
+      },
+      deleteBook(id){
+        // 方法一 findIndex加下标
+        // let index=this.books.findIndex(function (item) {
+        //   return item.id===id;
+        // });
+        // this.books.splice(index,1);
+        // 方法二 filter函数
+       this.books= this.books.filter(function (item) {
+          return item.id!==id;
+        });
       },
     },
+    //过滤器
+    filters:{
+      //过滤时间
+      format(value){
+        return value.getFullYear()+'-'+value.getMonth()+'-'+value.getDate()+' '+value.getHours()+':'+
+            value.getMinutes()+':'+value.getSeconds();
+      },
+    },
+    //自定义指令
+    directives:{
+      //获取焦点函数
+      focus: {
+        // 指令的定义
+        inserted: function (el) {
+          // 聚焦元素
+          el.focus()
+        }
+      },
+    },
+    //计算属性
+    computed:{
+      total(){
+        return this.books.length;
+      }
+    },
+    //侦听属性
+    watch:{
+      name(val){
+        let flag=this.books.some(function(item){
+          return item.name===val;
+        });
+        this.flagSubmit = flag === true;
+      }
+    }
+    
   }
 </script>
 
@@ -80,6 +159,13 @@
     margin:10px auto;
     width: 550px;
     text-align: center;
+  }
+  
+  .grid .total{
+    height: 30px;
+    line-height: 30px;
+    background-color: orange;
+    border-top: 2px solid #000000;
   }
   
   .grid table{
